1ace60a1be04f3907caf8a1b3ec68870901e2d58
Himal sharma <himal28924@gmail.com>
2024-03-19 09:08:35 +0100

    Now can move from one location to another

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import './App.css';
+import {RouterProvider, createHashRouter} from "react-router-dom";
 
 import Root from "./ui/pages/Root";
+import {HomePage} from "./ui/pages/HomePage";
+import {Parties} from "./ui/pages/Parties";
+
+const router = createHashRouter([
+    {
+        path: "/",
+        element: <Root/>,
+        children: [
+            {
+                path: "/",
+                element: <HomePage/>
+            },
+            {
+                path: "/parties",
+                element: <Parties/>
+            },
+        ],
+    },
+]);
+
+
 function App() {
   return (
-    <div className="App">
-      <Root/>
-    </div>
+      <div className="App">
+          <RouterProvider router={router}/>
+      </div>
   );
 }
 
