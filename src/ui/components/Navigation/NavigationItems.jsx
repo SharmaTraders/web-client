c8143a9561f33038bb770378e50a7a6e64cfb971
Himal sharma <41054866+himal28924@users.noreply.github.com>
2024-03-20 18:07:47 +0100

    BP-41 :  Implement the Naviagation bar (#3)
    ---------
    
    Co-authored-by: Sachin Baral <82201769+MrSachin7@users.noreply.github.com>

---
+++ src/ui/components/Navigation/NavigationItems.jsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import ListSubheader from '@mui/material/ListSubheader';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import PeopleIcon from '@mui/icons-material/People';
+import AssignmentIcon from '@mui/icons-material/Assignment';
+import ReceiptIcon from '@mui/icons-material/Receipt';
+import CurrencyRupeeIcon from '@mui/icons-material/CurrencyRupee';
+import MoneyOffIcon from '@mui/icons-material/MoneyOff';
+import CategoryIcon from '@mui/icons-material/Category';
+import {Link} from 'react-router-dom';
+import {Fragment} from "react";
+
+function mainListItems() {
+    return (
+        <Fragment>
+
+            <ListItemButton component={Link} to={"/"}>
+                <ListItemIcon>
+                    <DashboardIcon/>
+                </ListItemIcon>
+                <ListItemText primary="Dashboard"/>
+            </ListItemButton>
+
+            <ListItemButton component={Link} to={"/parties"}>
+                <ListItemIcon>
+                    <PeopleIcon/>
+                </ListItemIcon>
+                <ListItemText primary="Parties"/>
+            </ListItemButton>
+
+            <ListItemButton component={Link} to={"/items"}>
+                <ListItemIcon>
+                    <CategoryIcon/>
+                </ListItemIcon>
+                <ListItemText primary="Items"/>
+            </ListItemButton>
+
+            <ListItemButton component={Link} to={"/purchase"}>
+                <ListItemIcon>
+                    <ShoppingCartIcon/>
+                </ListItemIcon>
+                <ListItemText primary="Purchase"/>
+            </ListItemButton>
+
+            <ListItemButton component={Link} to={"/sales"}>
+                <ListItemIcon>
+                    <ReceiptIcon/>
+                </ListItemIcon>
+                <ListItemText primary="Sales"/>
+            </ListItemButton>
+
+            <ListItemButton component={Link} to={"/expenses"}>
+                <ListItemIcon>
+                    <MoneyOffIcon/>
+                </ListItemIcon>
+                <ListItemText primary="Expenses"/>
+            </ListItemButton>
+
+            <ListItemButton component={Link} to={"/income"}>
+                <ListItemIcon>
+                    <CurrencyRupeeIcon/>
+                </ListItemIcon>
+                <ListItemText primary="Income"/>
+            </ListItemButton>
+        </Fragment>
+    )
+}
+
+function secondaryListItems() {
+    return (
+        <Fragment>
+
+            <ListSubheader component="div" inset>
+                Reports
+            </ListSubheader>
+
+            <ListItemButton>
+                <ListItemIcon>
+                    <AssignmentIcon/>
+                </ListItemIcon>
+                <ListItemText primary="Day Book"/>
+            </ListItemButton>
+
+            <ListItemButton>
+                <ListItemIcon>
+                    <AssignmentIcon/>
+                </ListItemIcon>
+                <ListItemText primary="Purchase Reports"/>
+            </ListItemButton>
+
+            <ListItemButton>
+                <ListItemIcon>
+                    <AssignmentIcon/>
+                </ListItemIcon>
+                <ListItemText primary="Sell Reports"/>
+
+            </ListItemButton>
+        </Fragment>
+    )
+}
+
+export {mainListItems, secondaryListItems}
