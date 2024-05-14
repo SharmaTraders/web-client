4c5b89d6503f8b974d54fba000f6e1689d8e2cea
MrSachin7 <82201769+MrSachin7@users.noreply.github.com>
2024-05-14 18:24:39 +0200

    package json update

--- src/redux/features/api/stockApi.js
+++ src/redux/features/api/stockApi.js
@@ -47,35 +47,6 @@ const stockApi = baseApi
                             pageSize
                         }) => `item/${itemId}/stocks?pageNumber=${pageNumber}&pageSize=${pageSize}`,
 
-                // Only have one cache per item regardless of the pageNumber
-                // // Merge incoming data for infinite pagination
-                // merge: (currentCacheData, response) => {
-                //     // Initialize an object to keep track of existing stocks by their ID
-                //
-                //     if (response.pageNumber === 1) return response;
-                //     const existingStocksById = {};
-                //
-                //     // Populate existingStocksById with stocks from currentCacheData
-                //     currentCacheData.stocks?.forEach(stock => {
-                //         existingStocksById[stock.id] = true;
-                //     });
-                //
-                //     // Filter out response.stocks that are not already in the cache
-                //     const uniqueNewStocks = response.stocks?.filter(stock => !existingStocksById[stock.id]);
-                //
-                //     // Concatenate the unique new stocks with the existing stocks
-                //     currentCacheData.stocks = [...(currentCacheData.stocks ?? []), ...uniqueNewStocks];
-                //     currentCacheData.pageNumber = response.pageNumber;
-                //     currentCacheData.totalPages = response.totalPages;
-                //     currentCacheData.pageSize = response.pageSize;
-                //     return currentCacheData;
-                // },        serializeQueryArgs ({endpointName, queryArgs}) {
-                //     const obj = {itemId: queryArgs.itemId}
-                //     return endpointName + JSON.stringify(obj);
-                // },
-                //
-
-
 
                 // Refetch the data if the current page number is different from the previous page number
                 forceRefetch({currentArg, previousArg}) {
