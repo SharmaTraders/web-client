1898012ab8a484185444819f80b90833850cd2de
MrSachin7 <82201769+MrSachin7@users.noreply.github.com>
2024-05-19 13:14:50 +0200

    Fix some pr issues plus bit progression with income display

--- src/redux/features/api/income.js
+++ src/redux/features/api/income.js
@@ -1,28 +1,48 @@
 import {baseApi} from "./setup";
 
 const incomeApi = baseApi.injectEndpoints({
-    endpoints: (builder) => ({
+        endpoints: (builder) => ({
 
-        registerIncome: builder.mutation({
-            query: ({
-                        billingPartyId,
-                        date,
-                        remarks,
-                        amount,
-                    }) => ({
-                url: 'income',
-                method: 'POST',
-                body: {
-                    billingPartyId,
-                    date,
-                    remarks,
-                    amount
+            getIncomesByBillingParty: builder.query({
+                query: ({
+                            billingPartyId,
+                            pageNumber,
+                            pageSize
+                        }) => `billing-party/${billingPartyId}/incomes?pageNumber=${pageNumber}&pageSize=${pageSize}`,
+
+                forceRefetch({currentArg, previousArg}) {
+                    return currentArg !== previousArg;
+                },
+
+                providesTags: (result, error, arg) => {
+                    return [{type: 'Incomes', id: arg.billingPartyId}];
                 }
             }),
 
-            invalidatesTags: ['Incomes', 'BillingParty']
+            registerIncome: builder.mutation({
+                query: ({
+                            billingPartyId,
+                            date,
+                            remarks,
+                            amount,
+                        }) => ({
+                    url: 'income',
+                    method: 'POST',
+                    body: {
+                        billingPartyId,
+                        date,
+                        remarks,
+                        amount
+                    }
+                }),
+
+                invalidatesTags: (result, error, arg) => [ {
+                    type: "Incomes",
+                    id: arg.billingPartyId
+                },'Incomes', 'BillingParty']
+            })
         })
     })
-});
+;
 
-export const {useRegisterIncomeMutation} = incomeApi;
+export const {useGetIncomesByBillingPartyQuery,useRegisterIncomeMutation} = incomeApi;
