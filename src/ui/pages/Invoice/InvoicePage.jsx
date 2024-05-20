81e1fc5bc2fdbd84c345eea4ba9645679db29d10
MrSachin7 <82201769+MrSachin7@users.noreply.github.com>
2024-05-20 13:19:54 +0200

    Fix merge issues

--- src/ui/pages/Invoice/InvoicePage.jsx
+++ src/ui/pages/Invoice/InvoicePage.jsx
@@ -1,29 +1,15 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {
-    TextField,
     Button,
     Grid,
     Typography,
-    IconButton,
-    Box,
-    InputAdornment,
-    Checkbox,
-    FormControlLabel
+
 } from '@mui/material';
-import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
-import {useGetItemsQuery} from "../../../redux/features/api/itemApi";
-import BillingPartyAutocomplete from "../../components/BillingParty/BillingPartyAutocomplete";
-import ItemAutocomplete from "../../components/Item/ItemAutocomplete";
-import Calendar from "@sbmdkl/nepali-datepicker-reactjs";
+
 import '@sbmdkl/nepali-datepicker-reactjs/dist/index.css';
-import AddIcon from "@mui/icons-material/Add";
-import DeleteIcon from '@mui/icons-material/Delete';
-import {toast} from "react-toastify";
-import {useAddPurchaseMutation} from "../../../redux/features/api/purchaseApi";
-import {useAddSaleMutation} from "../../../redux/features/api/saleApi"; // Import the sale mutation
+
 import "./InvoicePage.css";
 import {InvoiceActivityComponent} from "../../components/Invoice/InvoiceActivityComponent";
-import Button from "@mui/material/Button";
 import {useNavigate} from "react-router-dom";
 
 function InvoicePage({mode}) {
